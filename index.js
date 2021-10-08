c56ab7835001b73dedd6173c71dcb1fa08533f65
alxsandre <alexandr.foret@gmail.com>
2021-10-08 10:47:37 +0200

    Docker and MySQL connected

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config()
+const { MYSQL_IP, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD } = require("./config/config");
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : MYSQL_IP,
+    port     : MYSQL_PORT,
+    user     : MYSQL_USER,
+    password : MYSQL_PASSWORD
+  });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+  });
+connection.end();
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h2>Hi There!</h2>");
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
