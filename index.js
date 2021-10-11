f08caaf2ee954d826a25a3c7c03e3740610e60bf
alxsandre <alexandr.foret@gmail.com>
2021-10-11 09:52:03 +0200

    depends database and retry

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const { MYSQL_IP, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD } = require("./config/c
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : MYSQL_IP,
-    port     : MYSQL_PORT,
-    user     : MYSQL_USER,
-    password : MYSQL_PASSWORD
-  });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connecté à la base de données MySQL!");
-  });
-connection.end();
 
+const connectWithRetry = () => {
+    var connection = mysql.createConnection({
+        host     : MYSQL_IP,
+        port     : MYSQL_PORT,
+        user     : MYSQL_USER,
+        password : MYSQL_PASSWORD
+      });
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connecté à la base de données MySQL!");
+        //setTimeout(connectWithRetry, 5000)
+      });
+    connection.end();
+}
+
+connectWithRetry();
 
 const app = express();
 
